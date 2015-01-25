bc159b3d4949856755af806f3b2bb8cb886e4a74
mrivas <mrivas@users.noreply.github.com>
2015-01-25 02:08:46 -0800

    New table with cufflinks & aligRate

--- source/figures/mouse/procedure.sh
+++ source/figures/mouse/procedure.sh
@@ -1,28 +1,58 @@
+# FASTQ files
 #for file in /data/rivasas2/limbs/reads_clean/mouse/*trimmed.clipped.fastq*; do ln -s $file .;done
+# SAM files
 #for file in /data/rivasas2/limbs/alignment_clean/mouse/*sorted.sam; do ln -s $file .; done
 
-# FPKM
-for feature in genes isoforms; do
-for file in /data/rivasas2/limbs/cufflinks_clean/mouse/cuffdiff.*/${feature}.fpkm_tracking; do
-   prefix=$(echo $file | awk '{n=split($0,a,"/");split(a[n-1],b,".");print b[2]}')
-   ln -s $file ${prefix}_${feature}.fpkm_tracking
-done; done
-# Differences
-for feature in gene isoform; do
-for file in /data/rivasas2/limbs/cufflinks_clean/mouse/cuffdiff.*/${feature}_exp.diff; do
-   prefix=$(echo $file | awk '{n=split($0,a,"/");split(a[n-1],b,".");print b[2]}')
-   ln -s $file ${prefix}_${feature}_exp.diff
-done; done
-
-rm summary.txt
+specie="mouse"
+for limb in FL HL; do
 for stage in W2 W3_4 W6; do
-	echo Mouse_${stage} \
-	",\`genes_FPKM <https://132.239.135.28/public/limbs/files/mouse/"${stage}_genes.fpkm_tracking">\`_" \
-	",\`isoforms_FPKM <https://132.239.135.28/public/limbs/files/mouse/"${stage}_isoforms.fpkm_tracking">\`_" \
-	",\`gene_diff <https://132.239.135.28/public/limbs/files/mouse/"${stage}_gene_exp.diff">\`_" \
-	",\`isoform_diff <https://132.239.135.28/public/limbs/files/mouse/"${stage}_isoform_exp.diff">\`_" \
-	>> summary.txt
-done
+rep=0
+for fastq in $(ls /data/rivasas2/limbs/reads_clean/mouse/*trimmed.clipped.fastq* | grep $limb | grep $stage);do
+	rep=$(($rep+1))
+	name=$(echo $fastq | awk '{n=split($0,a,"/");split(a[n],b,".trimmed");print b[1]}')
+	alias=mouse_${limb}_${stage}_${rep}
+	
+	# Link fastq files
+	#ln -s ${fastq} .
+	fastqLink="\`fastq <https://132.239.135.28/public/limbs/files/mouse/${name}.trimmed.clipped.fastq.gz>\`_"
+	
+	# Link sam files
+	# ln -s /data/rivasas2/limbs/alignment_clean/mouse/${name}.trimmed.clipped.sorted.sam .
+	samLink="\`sam <https://132.239.135.28/public/limbs/files/mouse/${name}.trimmed.clipped.sorted.sam>\`_"
+	aligRate=$(grep ${name} /data/rivasas2/limbs/alignment_clean/mouse/mapped_reads.txt | awk '{print $3/$2}')
+	
+	# Link cufflinks gene expression files
+	#ln -s /data2/rivasas2/limbs/cufflinks_time_series/mouse/cufflinks_${name}/genes.fpkm_tracking cufflinks_${name}.genes.fpkm_tracking
+	cufflinkLink="\`genes_fpkm <https://132.239.135.28/public/limbs/files/mouse/cufflinks_${name}.genes.fpkm_tracking>\`_"
+
+
+	# Table summary
+	echo -e $name","${alias}","${fastqLink}","${samLink}","${aligRate}","${cufflinkLink}
+done; done;done
+
+
+# Cuffdiff files
+#for feature in genes isoforms; do
+#for file in /data/rivasas2/limbs/cufflinks_clean/mouse/cuffdiff.*/${feature}.fpkm_tracking; do
+#   prefix=$(echo $file | awk '{n=split($0,a,"/");split(a[n-1],b,".");print b[2]}')
+#   ln -s $file ${prefix}_${feature}.fpkm_tracking
+#done; done
+## Differences
+#for feature in gene isoform; do
+#for file in /data/rivasas2/limbs/cufflinks_clean/mouse/cuffdiff.*/${feature}_exp.diff; do
+#   prefix=$(echo $file | awk '{n=split($0,a,"/");split(a[n-1],b,".");print b[2]}')
+#   ln -s $file ${prefix}_${feature}_exp.diff
+#done; done
+#
+#rm summary.txt
+#for stage in W2 W3_4 W6; do
+#	echo Mouse_${stage} \
+#	",\`genes_FPKM <https://132.239.135.28/public/limbs/files/mouse/"${stage}_genes.fpkm_tracking">\`_" \
+#	",\`isoforms_FPKM <https://132.239.135.28/public/limbs/files/mouse/"${stage}_isoforms.fpkm_tracking">\`_" \
+#	",\`gene_diff <https://132.239.135.28/public/limbs/files/mouse/"${stage}_gene_exp.diff">\`_" \
+#	",\`isoform_diff <https://132.239.135.28/public/limbs/files/mouse/"${stage}_isoform_exp.diff">\`_" \
+#	>> summary.txt
+#done
 
 #rm summary.txt
 #for stage in W2 W3_4 W6; do
--- source/index.rst
+++ source/index.rst
@@ -14,6 +14,18 @@ Contents:
 
    betweenSpecies
 
+New analyses
+============
+
+Pig, fill in all stages
+Opossum, forelimbs
+
+Differentially expressed genes on:
+fore and hinlimbs on each species
+fore and hinlimbs betwee all species
+transcriptome divergence among stages
+
+
 .. Indices and tables
 .. ==================
 .. 
